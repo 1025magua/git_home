4a745506624507f433f35c87b159491d733ea53b
lee <lee@163.com>
2022-03-03 17:40:23 +0800

    modify file

--- test_runner.py
+++ test_runner.py
@@ -3,4 +3,3 @@ import os
 
 if __name__ == '__main__':
     pytest.main(['-vs', './testcases/', '--alluredir=report/allure_json'])
-    os.system('allure generate report/allure_json -o report/allure_report --clean')
