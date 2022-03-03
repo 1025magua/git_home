3073cb7de0253e12d82bb9c778277631b7366921
lee <lee@163.com>
2022-03-03 16:41:29 +0800

    delete file

--- test_runner.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import os
-
-if __name__ == '__main__':
-    pytest.main(
-        ['-vs', './testcases/test_business_token_login02/test_login05.py', './testcases/test_my_interface01',
-         '--alluredir=report/allure_json'])
-    os.system('allure generate report/allure_json -o report/allure_report --clean')
